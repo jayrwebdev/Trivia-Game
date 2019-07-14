e7cebc5d7e930e07e046a7d46defbdfe8c3a4861
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-07-14 17:17:54 -0400

    css changes, additions to the javascript

--- js/trivia.js
+++ js/trivia.js
@@ -1,5 +1,5 @@
 // Objects Array
-var triviaQuestions = [{
+var triviaQ = [{
     question: 'Who is the captain of the Straw Hat Pirates?',
     options: ['Franky', 'Luffy', 'Robin', 'Nami', 'Jimbei'],
     name: 'Captain',
@@ -16,25 +16,45 @@ var triviaQuestions = [{
 },
 {
     question: 'Who is Vice-Captain of the Straw Hat Pirates?',
-    options:['Usopp', 'Sanji', 'Zoro', 'Chopper', 'Brook'],
-    name:'coCaptain',
-    answers:'Zoro',
-    divclass:'viceCaptain',
+    options: ['Usopp', 'Sanji', 'Zoro', 'Chopper', 'Brook'],
+    name: 'coCaptain',
+    answers: 'Zoro',
+    divclass: 'viceCaptain',
 },
 {
-    question: 'Who did the Straw Hats rescue at Enies Lobby?' ,
+    question: 'Who did the Straw Hats rescue at Enies Lobby?',
     options: ['Robin', 'Franky', 'Ussop', 'Zoro', 'Sanji'],
     name: 'strawRescue',
     answers: 'Robin',
-    divclass:'eniesLobby',
+    divclass: 'eniesLobby',
 },
 {
-    question:  'What is the name of the Straw Hats Pirate Ship?',
+    question: 'What is the name of the Straw Hats Pirate Ship?',
     options: ['Going Merry', 'Moby Dick', 'Thousand Sunny', 'Oro Jackson', 'Red Force'],
     name: 'strawShip',
     answers: 'Thousand Sunny',
     divclass: "pirateShip",
 }
 ]
+var labels = ['First','Second','Third','Fourth','Fifth']
 // Post questions & options and answers to html
-    
+
+   $("#startButon").on('click', function () {
+          $(".start").hide();
+   })
+// show questions
+   function showQuestions() {
+       for ( var t = 0 ; t < triviaQ.length; t++) {
+           
+       }   
+        // loops through answers for each  button
+       for (var i = 0; i <= 5; i++) {
+            
+       }
+       
+   }
+   function timer() {
+
+   }
+
+
